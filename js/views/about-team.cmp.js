e16f71d644461aa99d878dcb1066c2296b9182d7
Amitai Bassist <131amitaibasi@gmail.com>
2022-11-09 08:32:09 +0200

    final starter

---
+++ js/views/about-team.cmp.js
@@ -0,0 +1,9 @@
+export default {
+    template: `
+    <section class="about-team">
+        <h2>Our team- Amitai Bassist and Efrat </h2>
+        <img src="./img/ca-logo-dark1.png" alt="" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita nemo voluptate dignissimos ab quibusdam recusandae, suscipit soluta. In reprehenderit repellat commodi ab eligendi. Nulla, inventore ipsum voluptatem ipsam quidem alias?</p>
+    </section>
+    `
+}
